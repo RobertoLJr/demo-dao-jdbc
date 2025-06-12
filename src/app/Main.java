4162ad418436b64a9df0e6058aebaeaedecacda2
Roberto Junior <gsn.roberto@gmail.com>
2025-06-12 11:47:27 -0300

    feat: add Department class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,12 @@
 package app;
 
+import model.entities.Department;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
