705ff9459d9b6f8e910171c7f36516be62a3df0e
Roberto Junior <gsn.roberto@gmail.com>
2025-06-12 12:41:22 -0300

    feat: implement SellerDaoJDBC.findById()

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,21 +2,16 @@ package app;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(1991, 1, 23), 3000.0, obj);
-        System.out.println(seller);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
